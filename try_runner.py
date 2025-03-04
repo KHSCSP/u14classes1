2c6eac8b7d5eb61586f1e709753a472ac961f736
heyscottn <heyscottn@gmail.com>
2025-03-04 14:03:58 -0600

    mm

--- try_runner.py
+++ try_runner.py
@@ -2,18 +2,18 @@
 # a class is like a blueprint
 # from that blueprint we can create many objects
 
-from car_class import Car
+from car import Car
 
 
 
-from student_class import Student
+from student import Student
 
 
 
-from state_class import State
+from state import State
 
 
 
 
-from animal_class import Animal
+from animal import Animal
 
